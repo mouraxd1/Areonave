e6e01bb99a7827510b5fe0ea375346dd455f5ec0
mouraxd1 <mouraxd1001@gmail.com>
2022-08-18 11:06:18 -0300

    vader

--- Contexts/VoeAirlinesContext.cs
+++ Contexts/VoeAirlinesContext.cs
@@ -3,11 +3,22 @@ using VoeAirlinesSenai.Entities;
 
 namespace VoeAirlinesSenai.Entities;
 
-public class VoeAirlinesContext : DbContext{
+public class VoeAirlinesContext : DbContext
+{
+    private readonly IConfiguration _configuration;
 
-public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
-public DbSet<Manuntecao> Manutencoes => Set<Manuntecao>();
-public DbSet<Piloto> Pilotos => Set<Piloto>();
-public DbSet<Voo> Voos => Set<Voo>();
-public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
+    public VoeAirlinesContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
+    public DbSet<Manuntecao> Manutencoes => Set<Manuntecao>();
+    public DbSet<Piloto> Pilotos => Set<Piloto>();
+    public DbSet<Voo> Voos => Set<Voo>();
+    public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("VoeAirlines"));
+    }
 }
